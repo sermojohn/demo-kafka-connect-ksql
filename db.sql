fc5fac9f1612e16efcf71e789aa0078ae32f7399
Ioannis Sermetziadis <ioannis@codebgp.com>
2022-11-09 16:57:44 +0200

    experiment with keys and tombstone

--- db.sql
+++ db.sql
@@ -1,7 +1,22 @@
 create schema demo;
 
-create table consoles(id integer PRIMARY KEY, name varchar(255), timestamp TIMESTAMPTZ);
+-- deletion not detected
+create table consoles(id integer PRIMARY KEY, name varchar(255), timestamp TIMESTAMPTZ, ignore boolean);
 
+-- deletions detected using soft-deletion
+create table consoles_with_softdeletion(id integer PRIMARY KEY, name varchar(255), timestamp TIMESTAMPTZ, del_timestamp TIMESTAMPTZ);
+alter table consoles_with_softdeletion add column ignore boolean default false;
+alter table consoles_with_softdeletion drop column ignore;
+
+-- target tables
 create table copy_consoles(id integer PRIMARY KEY, name varchar(255), timestamp TIMESTAMPTZ);
+create table copy_consoles_with_softdeletion(id integer PRIMARY KEY, name varchar(255), timestamp TIMESTAMPTZ, del_timestamp TIMESTAMPTZ);
+alter table copy_consoles_with_softdeletion add column ignore boolean default false;
+
+
+
+-- sample data
 -- insert into consoles (id, name, timestamp) values (1, 'Amiga', now());
+
+insert into consoles_with_softdeletion (id, name, timestamp) values (1, 'Amiga', now());
 
